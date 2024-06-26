06611d68b63e01bf3f0a69c86396524ca27a84a5
PandelisMargaronis <pmargaro@hamilton.edu>
2024-06-26 12:07:17 -0400

    sorted muscle group badges, made them look more appealing

--- src/components/SessionsPage.js
+++ src/components/SessionsPage.js
@@ -117,6 +117,41 @@ const SessionsPage = () => {
 
   return (
     <div className="sessions-page-container container-fluid d-flex flex-column">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Sessions
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link">
+                | <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Sessions
+              </a>
+            </li>
+            {/* <li className="nav-item">
+              <a className="nav-link" href="#">
+                Sessions
+              </a>
+            </li> */}
+          </ul>
+        </div>
+      </nav>
       <div className="row mt-5 flex-grow-1">
         <div className="top-cont-sessions col-12">
           <div className="card mb-4">
